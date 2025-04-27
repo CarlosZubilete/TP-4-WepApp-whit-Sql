70dac34b05cf880b30450377dc2bc23962dd8414
CZubileteSanchez <carloszubilete@gmail.com>
2025-04-27 13:52:44 -0300

    Creamos WebApp-neptuno, en donde a travez de un control de filtro , obtenmos los prodcutos (todos o filtrados)

---
+++ WebApp-SQL-tp4/WebApp-Neptuno.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page 
+    Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="WebApp-Neptuno.aspx.cs" 
+    Inherits="WebApp_SQL_tp4.WebApp_Neptuno" 
+    UnobtrusiveValidationMode="None"
+    %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Base de datos - Neptuno </h1>
+    <form id="form1" runat="server">
+        <%-- FILTRO ID PRODUCTO --%>
+        <div>
+            <span>Id Producto: </span>
+            <asp:DropDownList runat="server" ID="ddlIdProduct" 
+                AutoPostBack="true"
+                OnSelectedIndexChanged="ddlIdProduct_SelectedIndexChanged">
+                <asp:ListItem Enabled="True" Value="0">Igual a:</asp:ListItem>
+                <asp:ListItem Enabled="True" Value="1">Mayor a: </asp:ListItem>
+                <asp:ListItem Enabled="True" Value="2">Menor a:</asp:ListItem>
+            </asp:DropDownList>
+            <asp:TextBox runat="server" ID="txtIdProduct"></asp:TextBox>
+            <asp:RangeValidator ID="rangeIdProduct" runat="server" ControlToValidate="txtIdProduct" MaximumValue="77" MinimumValue="1" Type="Integer">No se encontraron resultados</asp:RangeValidator>
+        </div>
+        <%-- Filtro ID CATRGORIA --%>
+        <div>
+            <span>Id Categoria: </span>
+            <asp:DropDownList runat="server" ID="DropDownList1" 
+                AutoPostBack="true">
+                <asp:ListItem Enabled="True" Value="0">Igual a:</asp:ListItem>
+                <asp:ListItem Enabled="True" Value="1">Mayor a: </asp:ListItem>
+                <asp:ListItem Enabled="True" Value="2">Menor a:</asp:ListItem>
+            </asp:DropDownList>
+            <asp:TextBox runat="server" ID="txtIdCategory"></asp:TextBox>
+            <asp:RangeValidator ID="rangeIdCategory" runat="server" ControlToValidate="txtIdCategory" MaximumValue="8" MinimumValue="1" Type="Integer">No se encontraron resultados</asp:RangeValidator>
+        </div>
+            <asp:Button ID="btnFilter" runat="server" Text="Filtrar" OnClick="btnFilter_Click" />
+        <br />    
+            <asp:Label ID="lblShow" runat="server"></asp:Label>
+
+        <%-- GRID VIUW --%>
+        <h3>Tabla de Productos:</h3>
+        <asp:GridView ID="gvProducts" runat="server">
+        </asp:GridView>
+        <br />
+
+    </form>
+</body>
+</html>
---
+++ WebApp-SQL-tp4/WebApp-Neptuno.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApp_SQL_tp4
+{
+  public partial class WebApp_Neptuno : System.Web.UI.Page
+  {
+    private const string connectingString = @"Data Source=DESKTOP-LFTFVP5\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+    private string sqlConsulta = "Select IdProducto, NombreProducto, IdCategoría , CantidadPorUnidad, PrecioUnidad From Productos ";
+    
+    private void CargarProductos(string query)
+    {
+      using (SqlConnection connection = new SqlConnection(connectingString))
+      {
+        connection.Open();
+        SqlCommand command = new SqlCommand(query, connection);
+        SqlDataReader reader = command.ExecuteReader();
+        gvProducts.DataSource = reader;
+        gvProducts.DataBind();
+      }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (!IsPostBack)
+      {
+        CargarProductos(sqlConsulta); 
+      }
+      
+    }
+
+    private const string where = " Where ";
+    protected void ddlIdProduct_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      //txtIdProduct.Text = ddlIdProduct.SelectedItem.ToString();
+      //txtIdProduct.Text = ddlIdProduct.SelectedValue.ToString();
+      // int ddlValue = Convert.ToInt32(ddlIdProduct.SelectedValue.ToString());
+      //string consulta = this.getSqlConsulta(ddlValue, txtIdProduct.Text);
+    }
+
+    protected string getSqlConsulta(int operadorRacional)
+    {
+      /*
+        0 -> '=' (igual)
+        1 -> '>' - (mayor)
+        2 -> '<' - menor 
+        Where IdProducto >(operador logico) 5(input)
+      */
+      string consulta = "";
+      switch (operadorRacional)
+      {
+        case 0:
+          consulta = " IdProducto = ";
+          break;
+        case 1:
+          consulta = " IdProducto > ";
+          break;
+        case 2:
+          consulta = " IdProducto < ";
+          break;
+      }
+
+      return consulta;
+    }
+
+  
+    protected void btnFilter_Click(object sender, EventArgs e)
+    {
+      int ddlValue = Convert.ToInt32(ddlIdProduct.SelectedValue.ToString());
+
+      string sentenciaRacional = this.getSqlConsulta(ddlValue);
+
+      string idProduct = txtIdProduct.Text;
+      string queryIdFiltrado = sqlConsulta + where + sentenciaRacional + idProduct;
+
+      lblShow.Text = queryIdFiltrado; 
+
+      if(txtIdProduct.Text.Trim().Length > 0)
+      {
+        CargarProductos(queryIdFiltrado);
+      }
+      else
+      {
+        CargarProductos(sqlConsulta);
+      }
+
+    }
+  }
+}
+
+/*
+  SqlConnection sqlConnection = new SqlConnection(conectingString);
+  sqlConnection.Open();
+
+  SqlCommand sqlCommand = new SqlCommand(sqlConsulta,sqlConnection);
+
+  SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+  gvProducts.DataSource = sqlDataReader;
+  gvProducts.DataBind();
+
+  sqlConnection.Close();
+*/
